a0700698a7b3073ee17df2b2de8697c6a6763eeb
Leonard Polz <mail@leonardpolz.de>
2024-04-23 16:59:57 +0200

    Auto fmt

--- tf-governance-interceptor-facade/main.tf
+++ tf-governance-interceptor-facade/main.tf
@@ -1,4 +1,4 @@
 module "governance_interceptor" {
-    source = "git::https://github.com/leonardpolz/terraform-governance-framework-interceptor-module-example?ref=v1.0.0"
-    configurations = var.configurations
+  source         = "git::https://github.com/leonardpolz/terraform-governance-framework-interceptor-module-example?ref=v1.0.0"
+  configurations = var.configurations
 }
